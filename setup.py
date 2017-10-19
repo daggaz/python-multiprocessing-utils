fa0dba9a059c056185cb1ca7880326f29c2aae4f
Jamie Cockburn <jamiec@extramsp.com>
2017-10-19 17:33:00 +0100

    Added python 3.6 support

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     packages=find_packages(exclude=['tests']),
     install_requires=install_requires,
